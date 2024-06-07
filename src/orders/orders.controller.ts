842304ee435ab9b88433a3b1da0db8731bb21318
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-07 10:50:29 -0400

    fix: refactor

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,9 +1,4 @@
-import {
-  Controller,
-  NotImplementedException,
-  ParseIntPipe,
-  ParseUUIDPipe,
-} from '@nestjs/common';
+import { Controller, ParseUUIDPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
